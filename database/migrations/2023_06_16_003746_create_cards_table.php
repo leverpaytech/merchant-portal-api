85fc45d8dbb7c7dd6ad776d2e9ed974c4f1b7e8c
Ilelaboye Lekan <ilelaboyealekan@gmail.com>
2023-06-16 01:40:33 +0100

    card migration

---
+++ database/migrations/2023_06_16_003746_create_cards_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->bigInteger('card_number');
+            $table->string('cvv');
+            $table->string('pin');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+};
