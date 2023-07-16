423509f7adea32198677f9404c5efa46e25c7347
Abdul Kura <abdilkura@gmail.com>
2023-07-16 13:39:02 +0100

    Debugging

--- app/Models/Merchant.php
+++ app/Models/Merchant.php
@@ -9,5 +9,13 @@ class Merchant extends Model
 {
     use HasFactory;
 
+    
+    protected $fillable = [
+        'user_id',
+        'business_name',
+        'business_address',
+        'business_phone'
+    ];
+
     protected $hidden = ['id'];
 }
