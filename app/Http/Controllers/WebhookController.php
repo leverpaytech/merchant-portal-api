7ef562fe42a4af4a462ecc2b72cc88ccd6bd0dd1
Ilelaboye Lekan <ilelaboyealekan@gmail.com>
2023-10-22 10:43:34 +0100

    Update WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -56,102 +56,102 @@ class WebhookController extends Controller
         // DB::beginTransaction();
 
         // try {
-            $web = new Webhook;
-            $web->raw = json_encode($request->all());
-            $web->sessionId = $request['sessionId'];
-            $web->bankSessionId = $request['sessionId'];
-            $web->accountNumber = $request['accountNumber'];
-            $web->tranRemarks = $request['tranRemarks'];
-            $web->amount = $request['transactionAmount'];
-            $web->settledAmount = $request['settledAmount'];
-            $web->feeAmount = $request['feeAmount'];
-            $web->vatAmount = $request['vatAmount'];
-            $web->currency = $request['currency'];
-            $web->transRef = $request['initiationTranRef'];
-            $web->settlementId = $request['settlementId'];
-            $web->sourceAccountNumber = $request['sourceAccountNumber'];
-            $web->sourceAccountName = $request['sourceAccountName'];
-            $web->sourceBankName = $request['sourceBankName'];
-            $web->channelId = $request['channelId'];
-            $web->tranDateTime = $request['tranDateTime'];
-            $web->bank = 'providus';
-            $web->save();
+        $web = new Webhook;
+        $web->raw = json_encode($request->all());
+        $web->sessionId = $request['sessionId'];
+        $web->bankSessionId = $request['sessionId'];
+        $web->accountNumber = $request['accountNumber'];
+        $web->tranRemarks = $request['tranRemarks'];
+        $web->amount = $request['transactionAmount'];
+        $web->settledAmount = $request['settledAmount'];
+        $web->feeAmount = $request['feeAmount'];
+        $web->vatAmount = $request['vatAmount'];
+        $web->currency = $request['currency'];
+        $web->transRef = $request['initiationTranRef'];
+        $web->settlementId = $request['settlementId'];
+        $web->sourceAccountNumber = $request['sourceAccountNumber'];
+        $web->sourceAccountName = $request['sourceAccountName'];
+        $web->sourceBankName = $request['sourceBankName'];
+        $web->channelId = $request['channelId'];
+        $web->tranDateTime = $request['tranDateTime'];
+        $web->bank = 'providus';
+        $web->save();
 
-            $user = User::find($account->user_id);
+        $user = User::find($account->user_id);
 
-            $trans = new Transaction;
-            $trans->user_id = $account->user_id;
-            $trans->reference_no = $request['sessionId'];
-            $trans->tnx_reference_no=$request['initiationTranRef'];
-            $trans->amount =$request['transactionAmount'];
-            $trans->type = 'credit';
-            $trans->merchant = 'providus';
-            $trans->status = 1;
+        $trans = new Transaction;
+        $trans->user_id = $account->user_id;
+        $trans->reference_no = $request['sessionId'];
+        $trans->tnx_reference_no=$request['initiationTranRef'];
+        $trans->amount =$request['transactionAmount'];
+        $trans->type = 'credit';
+        $trans->merchant = 'providus';
+        $trans->status = 1;
 
 
-            if($account->type == 'investment'){
-                $invest = Investment::where('accountNumber', $request['accountNumber'])->first();
-                $invest->amount_paid = $request['transactionAmount'];
-                $invest->status = 1;
-                $invest->save();
-                $trans->balance = $user->wallet->withdrawable_amount;
-                $trans->transaction_details = json_encode([
-                    'investment_id'=>$invest->id,
-                ]);
-
-                $html = "
-                <p>Hello {$user['first_name']} {$user['last_name']},</p>
-                <p style='margin-bottom: 8px'>
-                    Your investment of {$request['transactionAmount']} was successful. Login into your account to track your investment
-                    </p>
-                ";
-
-                SmsService::sendMail('', $html, 'Investment Successful', $user['email']);
-
-            }else{
-                WalletService::addToWallet($user->id, $request['transactionAmount']);
-                $trans->balance = floatval($user->wallet->withdrawable_amount) + floatval($request['transactionAmount']);
-            }
-
-            $trans->extra = json_encode([
-                'webhook'=>json_encode($request->all())
+        if($account->type == 'investment'){
+            $invest = Investment::where('accountNumber', $request['accountNumber'])->first();
+            $invest->amount_paid = $request['transactionAmount'];
+            $invest->status = 1;
+            $invest->save();
+            $trans->balance = $user->wallet->withdrawable_amount;
+            $trans->transaction_details = json_encode([
+                'investment_id'=>$invest->id,
             ]);
-            $trans->save();
 
-            if($account->type == 'investment'){
-                $html = "<p style='margin-bottom: 8px'>
-                        Dear {$user->first_name},
-                    </p>
-                    <p style='margin-bottom: 10px'>An investment of ₦{$request['transactionAmount']} has been completed</p>
+            $html = "
+            <p>Hello {$user['first_name']} {$user['last_name']},</p>
+            <p style='margin-bottom: 8px'>
+                Your investment of {$request['transactionAmount']} was successful. Login into your account to track your investment
+                </p>
+            ";
 
-                    <p> Best regards, </p>
-                    <p> Leverpay </p>
-                ";
-                SmsService::sendMail('', $html, 'Invoice Completed', $user->email);
-                $account->accountNumber = rand(1000,9999).'_'.$request['accountNumber'];
-                $account->status = 0;
-                $account->save();
-            }else{
-                $html = "<p style='margin-bottom: 8px'>
-                        Dear {$user->first_name},
-                    </p>
-                    <p style='margin-bottom: 10px'>An amount of ₦{$request['transactionAmount']} has been credited to your wallet</p>
-                    <p style='margin-bottom: 2px'> Sender Account Number:  {$request['sourceAccountNumber']}</p>
-                    <p style='margin-bottom: 2px'> Sender Account Name:  {$request['sourceAccountName']}</p>
-                    <p style='margin-bottom: 2px'> Sender Bank Name:  {$request['sourceBankName']}</p>
-                    <p style='margin-bottom: 2px'> Date:  {$request['tranDateTime']}</p>
-                    <p> Best regards, </p>
-                    <p> Leverpay </p>
-                ";
-                SmsService::sendMail('', $html, 'Wallet Credit', $user->email);
-            }
+            SmsService::sendMail('', $html, 'Investment Successful', $user['email']);
 
-            return [
-                'requestSuccessful'=>true,
-                'sessionId'=>$request['sessionId'],
-                'responseMessage'=>'success',
-                'responseCode'=>'00'
-            ];
+        }else{
+            WalletService::addToWallet($user->id, $request['transactionAmount']);
+            $trans->balance = floatval($user->wallet->withdrawable_amount) + floatval($request['transactionAmount']);
+        }
+
+        $trans->extra = json_encode([
+            'webhook'=>json_encode($request->all())
+        ]);
+        $trans->save();
+
+        if($account->type == 'investment'){
+            $html = "<p style='margin-bottom: 8px'>
+                    Dear {$user->first_name},
+                </p>
+                <p style='margin-bottom: 10px'>An investment of ₦{$request['transactionAmount']} has been completed</p>
+
+                <p> Best regards, </p>
+                <p> Leverpay </p>
+            ";
+            SmsService::sendMail('', $html, 'Investment Completed', $user->email);
+            $account->accountNumber = rand(1000,9999).'_'.$request['accountNumber'];
+            $account->status = 0;
+            $account->save();
+        }else{
+            $html = "<p style='margin-bottom: 8px'>
+                    Dear {$user->first_name},
+                </p>
+                <p style='margin-bottom: 10px'>An amount of ₦{$request['transactionAmount']} has been credited to your wallet</p>
+                <p style='margin-bottom: 2px'> Sender Account Number:  {$request['sourceAccountNumber']}</p>
+                <p style='margin-bottom: 2px'> Sender Account Name:  {$request['sourceAccountName']}</p>
+                <p style='margin-bottom: 2px'> Sender Bank Name:  {$request['sourceBankName']}</p>
+                <p style='margin-bottom: 2px'> Date:  {$request['tranDateTime']}</p>
+                <p> Best regards, </p>
+                <p> Leverpay </p>
+            ";
+            SmsService::sendMail('', $html, 'Wallet Credit', $user->email);
+        }
+
+        return [
+            'requestSuccessful'=>true,
+            'sessionId'=>$request['sessionId'],
+            'responseMessage'=>'success',
+            'responseCode'=>'00'
+        ];
         // }catch(\Exception $e){
         //     DB::rollBack();
         //     return [
