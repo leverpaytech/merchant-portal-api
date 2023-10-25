562ac6e93b61099f5b6a9ddb188ad41db7aed19c
Michael Oshosanya <michael.oshosanya@deimos.co.za>
2023-10-25 17:58:43 +0100

    Finish complete payment endpoint

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\BaseController;
+use App\Models\Card;
+use App\Models\CardPayment;
 use App\Models\Transaction;
+use App\Services\CardService;
 use App\Services\SmsService;
+use App\Services\WalletService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\Uid\Ulid;
 use Webpatser\Uuid\Uuid;
 
 class CheckoutController extends BaseController
@@ -15,8 +20,13 @@ class CheckoutController extends BaseController
     public function createPayment(Request $request) {
         $this->validate($request, [
             'amount' => 'required',
-            'merchant_reference' => 'required'
+            'merchant_reference' => 'required',
+            'pan',
+            'cvv',
+            'expiry'
         ]);
+
+        $user = Auth::user();
 
         $walletBalance = Auth::user()->wallet->withdrawable_amount;
 
@@ -24,69 +34,83 @@ class CheckoutController extends BaseController
             $this->sendError('Insufficient balance to create payment');
         }
 
-        [$transactionHash, $transactionReference]= (function ($amount, $reference) {
+        if (!(new CardService)->validateCredentials(
+            $request->get('pan'),
+            $request->get('cvv'),
+            $request->get('pin'),
+            $request->get('expiry'),
+        )) {
+            $this->sendError('Card details incorrect');
+        }
 
-        })($request->get('amount'), $request->get('merchant_reference'));
+        $card = Card::where('user_id',Auth::id())
+            ->where('card_number', $request->get('pan'))
+            ->first();
+
+        $cardPayment = new CardPayment();
+        $cardPayment->uuid = Uuid::generate(4)->string;
+        $cardPayment->amount = $request->get('amount');
+        $cardPayment->card_id = $card->id;
+        $cardPayment->merchant_reference = $request->get('merchant_reference');
+        $cardPayment->payment_reference = 'LP_'.Ulid::generate();
+        $cardPayment->otp = random_int(1000,9999);
+        $cardPayment->save();
+
+        $content = "A card transaction with value {$request['amount']} has been initiated on your account, to verify your otp is: <br /> {$cardPayment->otp}";
+
+        SmsService::sendSms("Dear {$user->first_name},A card transaction with value {$request['amount']} has been initiated on your account, to verify your One-time Confirmation code is {$cardPayment->otp} and it will expire in 10 minutes. Please do not share For enquiry: contact@leverpay.io", '234'.$user->phone);
+        SmsService::sendMail("Dear {$user->first_name},", $content, "LeverPay Card Transaction OTP", $user->email);
 
         $this->successfulResponse([
-            'hash' => $transactionHash,
-            'transaction_reference' => $transactionReference,
+            'transaction_id' => $cardPayment->id,
             'amount' => $request->get('amount')
         ], 'Payment created');
 
     }
-    public function checkout(Request $request) {
-        $user = Auth::user();
-        $this->validate($request, [
-            'pan' => 'required',
-            'cvv' => 'required',
-            'pin' => 'required'
-        ]);
 
-        // Get transaction with hash
 
-        $transactionAmount = 0.00;
-        $walletBalance = Auth::user()->wallet->withdrawable_amount;
-
-        if ($transactionAmount > $walletBalance) {
-            $this->sendError('Insufficient balance');
-        }
-
-        $otp = random_int(1000,9999);
-
-        $content = "A card transaction with value {$request['amount']} has been initiated on your account, to verify your otp is: <br /> {$otp}";
-
-        SmsService::sendSms("Dear {$user->first_name},A card transaction with value {$request['amount']} has been initiated on your account, to verify your One-time Confirmation code is {$otp} and it will expire in 10 minutes. Please do not share For enquiry: contact@leverpay.io", '234'.$user->phone);
-        SmsService::sendMail("Dear {$user->first_name},", $content, "LeverPay Card Transaction OTP", $user->email);
-    }
-
-    public function completeTransaction(Request $request) {
+    public function completePayment(Request $request) {
         $this->validate($request, [
             'otp' => 'required|size:4',
-            'transaction_hash' => 'required'
+            'payment_id' => 'required'
         ]);
+
+        $cardPayment = CardPayment::where('uuid', $request->get('payment_id'))->firstOrFail();
+
+        if ($cardPayment->otp !== (integer)$request->get('otp')) {
+            $cardPayment->retries_left -= 1;
+            $cardPayment->save();
+
+            if ($cardPayment->retries_left == 0) {
+                $cardPayment->status = 'CANCELED';
+                $cardPayment->save();
+                return $this->sendError('Multiple incorrect OTP, transaction cancelled');
+            }
+            return $this->sendError('Incorrect OTP. '. $cardPayment->retries_left . " retries left");
+        }
 
 
         $user = Auth::user();
 
-        $transaction = [];
-
-        if ($request->get('otp') !== $transaction['otp']) {
-            return $this->sendError("Incorrect OTP.");
-        }
-
-        DB::transaction( function() use($transaction, $user) {
-            $ext = 'LP_' . Uuid::generate()->string;
+        DB::transaction( function() use($user, $cardPayment) {
             $transaction = new Transaction();
             $transaction->user_id = $user->id;
             $transaction->reference_no = 'LP_' . Uuid::generate()->string;
-            $transaction->tnx_reference_no = $ext;
-            $transaction->amount = $transaction['amount'];
-            $transaction->balance = floatval($user->wallet->withdrawable_amount) - floatval($transaction['amount']);
+            $transaction->tnx_reference_no = $cardPayment->payment_reference;
+            $transaction->amount = $cardPayment->amount;
+            $transaction->balance = floatval($user->wallet->withdrawable_amount) - floatval($cardPayment->amount);
             $transaction->type = 'debit';
-            $transaction->merchant = 'transfer';
+            $transaction->merchant = 'card_payment';
             $transaction->status = 1;
             $transaction->save();
+
+            WalletService::subtractFromWallet($user->id, $cardPayment->amount, 'naira');
+
+            $cardPayment->status = 'SUCCESSFUL';
+            $cardPayment->save();
+
         });
+
+        return $this->successfulResponse([], "Card payment successful");
     }
 }
