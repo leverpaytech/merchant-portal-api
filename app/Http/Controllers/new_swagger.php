1b69a2104103f181b81390134ccad4579d78c2f5
Abdul Kura <abdilkura@gmail.com>
2023-11-16 15:36:52 +0100

    merchant swagger documentation

--- app/Http/Controllers/new_swagger.php
+++ app/Http/Controllers/new_swagger.php
@@ -52,57 +52,6 @@ return [
 
             ],
         ],
-                /**merchant doc */
-		'v2' => [
-            'api' => [
-                'title' => 'API Documentation',
-                \App\Http\Middleware\EncryptCookies::class,
-                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-                \Illuminate\Session\Middleware\StartSession::class,
-                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-                \App\Http\Middleware\VerifyCsrfToken::class,
-                \Illuminate\Routing\Middleware\SubstituteBindings::class,
-                \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
-                'auth',
-            ],
-
-            'routes' => [
-                /*
-                 * Route for accessing api documentation interface
-                */
-                'api' => 'api/documentation/v2',
-            ],
-            'paths' => [
-                /*
-                 * Edit to include full URL in ui for assets
-                */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
-
-                /*
-                 * File name of the generated json documentation file
-                */
-                'docs_json' => 'api-docs1.json',
-
-                /*
-                 * File name of the generated YAML documentation file
-                */
-                'docs_yaml' => 'api-docs.yaml',
-
-                /*
-                * Set this to `json` or `yaml` to determine which documentation file to use in UI
-                */
-                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
-
-                /*
-                 * Absolute paths to directory containing the swagger annotations are stored.
-                */
-                'annotations' => [
-                    base_path('app'),
-                ],
-
-            ],
-        ],
-		/**merchant doc*/
     ],
     'defaults' => [
         'routes' => [
