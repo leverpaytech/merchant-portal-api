adca68b716cd41f8b9481412b1c7a6e632b69e63
Abdul <abdilkura@gmail.com>
2024-01-20 18:07:52 +0100

    swagger guard

--- app/Http/Controllers/SwaggerUiController.php
+++ app/Http/Controllers/SwaggerUiController.php
@@ -9,13 +9,14 @@ class SwaggerUiController extends Controller
 {
     public function index(Request $request)
     {
-        $this->middleware(['auth:api', 'your_custom_middleware']);
+        //$this->middleware(['auth:api', 'your_custom_middleware']);
 
         // Additional checks or actions here...
 
-        $user = Auth::user();
+        //$user = Auth::user();
 
-        return view('swagger.index', ['user' => $user]);
+        //return view('swagger.index', ['user' => $user]);
+        return view('swagger.index');
     }
 }
 
