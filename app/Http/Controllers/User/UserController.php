bcb7e25181df60c43de89a66d4df401a66572a78
Abdul <abdilkura@gmail.com>
2024-01-29 22:21:38 +0100

    vfd update

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1711,14 +1711,15 @@ class UserController extends BaseController
         
         $newBalance=($getLeverPayAccount->balance + $data['amount'])-$cashBack;
         
-        $payBillResult =VfdService::payBill($accessToken, $data['customerId'], $data['amount'], $data['division'], $data['paymentItem'], $data['productId'], $data['billerId'], $nin['referenceNo']);
+        // $payBillResult =VfdService::payBill($accessToken, $data['customerId'], $data['amount'], $data['division'], $data['paymentItem'], $data['productId'], $data['billerId'], $nin['referenceNo']);
         
-        $payBillResult=json_decode($payBillResult);
+        // $payBillResult=json_decode($payBillResult);
 
-        if ($payBillResult->status != '00') {
-            return response()->json('Transaction Failed, '.$payBillResult->message, 422);
-        }
-        $payToken=$payBillResult->token;
+        // if ($payBillResult->status != '00') {
+        //     return response()->json('Transaction Failed, '.$payBillResult->message, 422);
+        // }
+        //$payToken=$payBillResult->token;
+        $payToken="0022";
         // Start the database transaction
         DB::beginTransaction();
         try{
