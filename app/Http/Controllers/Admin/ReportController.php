e231cbd8c14a0ea12e12ca5c4b758a0fa6c96dda
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2024-02-25 14:31:24 +0100

    admin report wip

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class ReportController extends Controller
 {
-    //
+    public function getTodayTransactions(){
+        $users = DB::table('users')->join('transactions', 'transactions);
+    }
 }
