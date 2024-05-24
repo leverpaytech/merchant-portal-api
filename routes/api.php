b82c77c3f015fb2d533beb1cb94e4c6eaeaac1a1
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2024-05-24 10:54:22 +0100

    unblock bills payment

--- routes/api.php
+++ routes/api.php
@@ -183,7 +183,7 @@ Route::prefix('v1')->group( function(){
                     Route::get('get-biller-categories', [UserController::class,'billerCategories']);
                     Route::get('get-biller-list/{categoryName}', [UserController::class,'billerList']);
                     Route::get('get-biller-items/{billerId}/{divisionId}/{productId}', [UserController::class,'billerItems']);
-                    // Route::post('submit-bill-payment', [UserController::class,'billPayment']);
+                    Route::post('submit-bill-payment', [UserController::class,'billPayment']);
                     Route::post('create-new-pin', [UserController::class,'createBillPaymentPin']);
                     Route::post('reset-billpayment-pin', [UserController::class,'resetBillPaymentPin']);
                     Route::get('get-billpayments-history', [UserController::class,'viewBillPaymentHistory']);
@@ -222,7 +222,7 @@ Route::prefix('v1')->group( function(){
                 Route::get('get-biller-payment-items', [QuickTellerController::class,'getBillerPaymentItems']);
                 Route::get('get-biller-payment-items-by-amount', [QuickTellerController::class,'getBillerPaymentItemByAmount']);
                 Route::get('get-customer-transaction', [QuickTellerController::class,'getTransaction']);
-                // Route::post('submit-bill-payment', [QuickTellerController::class,'sendBillPayment']);
+                Route::post('submit-bill-payment', [QuickTellerController::class,'sendBillPayment']);
                 Route::post('validate-customer', [QuickTellerController::class,'validateCustomer']);
 
             });
