e96b685e97e4e2a791909b3d86cad9676a9dcf5c
Ilelaboye Lekan <40622920+ilelaboye@users.noreply.github.com>
2024-08-29 22:56:39 +0100

    dynamic account number changing cron job should run hourly and stop sending test email

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('accountnumber:timelimit')->everyMinute()->runInBackground();
+        $schedule->command('accountnumber:timelimit')->hourly()->runInBackground();
     }
 
     /**
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
